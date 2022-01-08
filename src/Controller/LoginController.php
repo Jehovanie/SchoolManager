8f52a619189edcccf988220b3f8d573adc36a000
Jehovanie <jehovanieram@gmail.com>
2022-01-09 00:31:34 +0300

    req validate email fixture create login ,user ,migration , define var EMAIL-DNS

---
+++ src/Controller/LoginController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class LoginController extends AbstractController
+{
+    /**
+     * @Route("/login", name="login")
+     */
+    public function index(AuthenticationUtils $authenitactionUtils): Response
+    {
+
+        // get the login error if there is one
+        $error = $authenitactionUtils->getLastAuthenticationError();
+
+        //last username enter by the user
+        $lastUsername = $authenitactionUtils->getLastUsername();
+
+        return $this->render('login/index.html.twig', [
+            'controller_name' => 'LoginController',
+            'last_username' => $lastUsername,
+            'error' => $error,
+        ]);
+    }
+}
